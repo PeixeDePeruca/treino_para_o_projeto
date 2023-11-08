705c85df9f1e372e65b5d3d1c9d5aa749fbc38a5
PeixeDePeruca <twitterluizin@gmail.com>
2023-11-08 17:30:49 -0300

    exemplos_projeto

---
+++ senac-project/pages/index.tsx
@@ -0,0 +1,13 @@
+import style from '@/styles/index.module.css';
+
+export default function Home() {
+  return (
+    <main>
+
+     <label>Email</label>
+     <br></br>
+     <input type="email" placeholder='digite o seu email.' />
+    </main>
+  )
+} 
+      
